25449edd8376d8bc6a33340679426769e31cdb43
Osumgba Chiamaka <osumgbachiamaka@gmail.com>
2019-09-20 14:43:49 +0100

    Task 3 done

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,6 @@
 
 const fetchApi = () => {
-    const api = 'http://www.somaku.com/users/1'
+    const api = 'https://jsonplaceholder.typicode.com/posts'
     fetch(api)
         .then(response => response.json())
         .then(data => showApi(data))
@@ -10,40 +10,27 @@ const fetchApi = () => {
 fetchApi()
 
 const showApi = (results) => {
-    //destructuring results
-    const {id, name, username, email, address, phone, website, company} = results
-    const {street, suite, city, zipcode, geo} = address
+    const table = document.querySelector('.profile table tbody')
 
-    //getting all company p
-    const companyDivs = document.querySelectorAll('.company p')
-    //destructuring all comapny p
-    const [companyName, companyCatchP, companyBs] = companyDivs
-
-    //getting all user p
-    const userDivs = document.querySelectorAll('.user p')
-    //destructuring all user Divs
-    const [one, two, three, four, five, six, seven] = userDivs
-
-    //getting all spans
-    const addressSpan = document.querySelectorAll('.user p span')
-    //destructuring all spans
-    const [spanOne, spanTwo, spanThree, spanFour, spanFive] = addressSpan
-
-    companyName.textContent = company.name;
-    companyCatchP.textContent = company.catchPhrase;
-    companyBs.textContent = company.bs;
-    
-    
-    one.textContent = id
-    two.textContent = name
-    three.textContent = username
-    four.textContent = email
-        spanOne.textContent = street
-        spanTwo.textContent = suite
-        spanThree.textContent = city
-        spanFour.textContent = zipcode
-        spanFive.textContent = `Longitude: ${geo.lng} Latitude: ${geo.lat}`
-    six.textContent = phone
-    seven.textContent = website
-    // console.log(one)
+    results.forEach((user) => {
+        const {userId, id, title, body} = user 
+        let tableRow = `
+            <tr>
+            <td class="pa3 bb b--black-10">
+                ${userId}
+            </td>
+            <td class="pa3 bb b--black-10">
+                ${id}
+            </td>
+            <td class="pa3 bb b--black-10">
+                ${title}
+            </td>
+            <td class="pa3 bb b--black-10">
+                ${body}
+            </td>
+            </tr>
+        `
+        table.insertAdjacentHTML("beforeend", tableRow)
+        console.log(userId, id, title)
+    })
 }
